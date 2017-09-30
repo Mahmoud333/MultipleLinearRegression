6c211252c0e26970bc4c7101f43a606397293c4e
Mahmoud333 <mahmoud_Smgl@hotmail.com>
2017-10-01 00:53:58 +0200

    python done

--- MultipleLinearRegression_DPShort.py
+++ MultipleLinearRegression_DPShort.py
@@ -26,19 +26,18 @@ y = dataset.iloc[:, 4].values               # 4 is the number of the column with
                                             #2nd- it might consider it as 0 < 1 < 2 so we have to make another good way
                                             #3rd- will use Dummy variables, will turn country column into table for each country (3 tables) with 0 and 1 / 0 if its not this country, 1 if its this country
                                             
-                                            
-                                            
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelEncoder_X = LabelEncoder() #create object
-X[:,0] = labelEncoder_X.fit_transform(X[:,0]) #apply this method on X all the lines and on its first column only then make all this = to our main X[:,0]
+X[:,3] = labelEncoder_X.fit_transform(X[:,3]) #apply this method on X all the lines and on its first column only then make all this = to our main X[:,0]
 
         # create X dummy variables, first France, 2nd Germany, 3rd Spain
-        
-onehotencoder = OneHotEncoder(categorical_features = [0]) #which column we would like to OneHotEncode
+
+onehotencoder = OneHotEncoder(categorical_features = [3]) #which column we would like to OneHotEncode
 X = onehotencoder.fit_transform(X).toarray()
 
 
-
+#-- Avoiding the Dummy Variable Trap [some librarys already takes care of it, put it to reminds you in future]
+X = X[ :, 1:]  #removed the first column of X, 1: means take all columns of X start from index 1 which is the 2nd column
 
 
 
@@ -48,10 +47,6 @@ from sklearn.cross_validation import train_test_split
                                              #test size is 20% of X and y
                                              #random_state we will have random results if we dont put it
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
-
-
-
-
 
 
 """                    
@@ -67,7 +62,14 @@ X_test = sc_X.transform(X_test)          #dont need to fit it bec. its alreay fi
                         #All the Varibales will be from -1 to +1
 """
 
+# Fitting Multiple Linear Regression to the Training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()      #create our object
+regressor.fit(X_train, y_train)
 
+
+# Predicting the Test set results
+y_pred = regressor.predict(X_test)
 
 
 
